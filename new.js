f4f0d847ba84d76a5e4bc349ffc83225f955931c
azlaan2605 <azzuazlaan@gmail.com>
2021-07-25 10:47:17 +0530

    javaS

---
+++ new.js
@@ -0,0 +1,23 @@
+// Function-
+
+function hey(a,b){
+    var sum=a+b;
+    return sum;
+}
+var sum1=hey(2,4);
+console.log(sum1);
+
+
+//Function exression-
+
+var add=function (a,b){
+    var sum=a+b;
+    return sum;
+}
+var sum2=add(4,5)
+console.log(sum2);
+
+
+
+
+
